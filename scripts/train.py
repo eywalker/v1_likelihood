37c0d9e9623d0552cf9de0336102dd00af61ac91
Edgar Y. Walker <edgar.walker@gmail.com>
2019-02-10 17:39:29 -0600

    Update script

--- scripts/train.py
+++ scripts/train.py
@@ -2,36 +2,36 @@
 from v1_likelihood import train, analysis, simulate_poisson, simulate_gaussian
 import datajoint as dj
 
-simulate_poisson.GaussTuningSet().fill()
-simulate_poisson.PoissonSimulation().populate(order='random', reserve_jobs=True)
-simulate_poisson.GTScores().populate(order='random', reserve_jobs=True)
-simulate_poisson.LinearRegression().populate(order='random', reserve_jobs=True)
-simulate_poisson.FitTuningCurves().populate(order='random', reserve_jobs=True)
-simulate_poisson.FittedPoissonScores().populate(order='random', reserve_jobs=True)
-simulate_poisson.FittedPoissonKL().populate(order='random', reserve_jobs=True)
-simulate_poisson.PoissonTrainedModel().populate(order='random', reserve_jobs=True)
-
-
-simulate_gaussian.GaussTuningSet().fill()
-simulate_gaussian.CorrelationMatrix().populate(order='random', reserve_jobs=True)
-simulate_gaussian.GaussianSimulation().populate(order='random', reserve_jobs=True)
-simulate_gaussian.GTScores().populate(order='random', reserve_jobs=True)
-simulate_gaussian.LinearRegression().populate(order='random', reserve_jobs=True)
-simulate_gaussian.FitTuningCurves().populate(order='random', reserve_jobs=True)
-simulate_gaussian.FittedPoissonScores().populate(order='random', reserve_jobs=True)
-simulate_gaussian.FittedPoissonKL().populate(order='random', reserve_jobs=True)
-simulate_gaussian.OptimalPoissonScores().populate(order='random', reserve_jobs=True)
-simulate_gaussian.OptimalPoissonKL().populate(order='random', reserve_jobs=True)
-simulate_gaussian.GaussianTrainedModel().populate(order='random', reserve_jobs=True)
+#simulate_poisson.GaussTuningSet().fill()
+#simulate_poisson.PoissonSimulation().populate(order='random', reserve_jobs=True)
+#simulate_poisson.GTScores().populate(order='random', reserve_jobs=True)
+#simulate_poisson.LinearRegression().populate(order='random', reserve_jobs=True)
+#simulate_poisson.FitTuningCurves().populate(order='random', reserve_jobs=True)
+#simulate_poisson.FittedPoissonScores().populate(order='random', reserve_jobs=True)
+#simulate_poisson.FittedPoissonKL().populate(order='random', reserve_jobs=True)
+#simulate_poisson.PoissonTrainedModel().populate(order='random', reserve_jobs=True)
+#
+#
+#simulate_gaussian.GaussTuningSet().fill()
+#simulate_gaussian.CorrelationMatrix().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.GaussianSimulation().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.GTScores().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.LinearRegression().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.FitTuningCurves().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.FittedPoissonScores().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.FittedPoissonKL().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.OptimalPoissonScores().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.OptimalPoissonKL().populate(order='random', reserve_jobs=True)
+#simulate_gaussian.GaussianTrainedModel().populate(order='random', reserve_jobs=True)
 
 #simulate_poisson.TrainedNetKL().populate(order='random', reserve_jobs=True)
 
 
-# #train.CVSet().populate(order='random', reserve_jobs=True)
-# restr = train.BinConfig & 'bin_counts = 91'
-# train.LinearRegression().populate(order='random', reserve_jobs=True)
-# analysis.LikelihoodStats.populate(order='random', reserve_jobs=True)
-# #train.RefinedCVTrainedModel().populate(restr, order='random', reserve_jobs=True)
+train.CVSet().populate(order='random', reserve_jobs=True)
+restr = train.BinConfig & 'bin_counts = 91'
+train.LinearRegression().populate(order='random', reserve_jobs=True)
+analysis.LikelihoodStats.populate(order='random', reserve_jobs=True)
+train.RefinedCVTrainedModel().populate(restr, order='random', reserve_jobs=True)
 #
 # train.BestRecoveredModel().populate(restr, order='random', reserve_jobs=True)
 #
